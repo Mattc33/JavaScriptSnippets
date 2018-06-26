7f2e93cb78cded61bd61c7abbef53cb142f0e4a9
Matt Chan <m.chan@obietechologies.com>
2018-06-26 16:36:22 -0400

    another .map example

--- FunctionalJS-Array/map/mapSimpleEx.js
+++ FunctionalJS-Array/map/mapSimpleEx.js
@@ -1,3 +1,10 @@
+// * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+// ? The map() method creates a new array with the results of calling 
+// ? a provided function on every element in the calling array.
+
+// ? take an array and turn it into an array of the same length but with modified values
+
 const animals = [
     { name: 'Fluffykins', species: 'rabbit' },
     { name: 'Caro',       species: 'dog' },
@@ -8,9 +15,7 @@ const animals = [
 ];
 
 // We want to get an array of all of the names of all the animals
-
 // Solve with a for loop first
-
 returnArrOfAnimalNames = function(arr) {
     const animalNameArr = [];
     for ( let i = 0; i<arr.length; i++ ) {
@@ -24,7 +29,6 @@ console.log(returnArrOfAnimalNames(animals));
 /*                                          
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
-
 // solve with map
 const names = animals.map(function(animal) {
     return animal.name;
@@ -37,5 +41,12 @@ console.log(names);
 const namesModified = animals.map(animal => animal.name + ' is a ' + animal.species);
 
 console.log(namesModified);
+
+// Map Multiply Each array item by 2
+const numArr = [1, 4, 9, 16];
+const multiplyItemsByTwo = (arr) => arr.map( (item) => item * 2); 
+console.log(multiplyItemsByTwo(numArr));
+
+
 
 
