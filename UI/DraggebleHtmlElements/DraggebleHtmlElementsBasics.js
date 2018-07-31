0cbc3daa92c4ebfea1a6c40d19ee18e38ad6eb00
Matt Chan <m.chan@obietechologies.com>
2018-07-31 18:10:41 -0400

    draggeble

---
+++ UI/DraggebleHtmlElements/DraggebleHtmlElementsBasics.js
@@ -0,0 +1,15 @@
+const dragElement = document.getElementById('dragElement');
+const dragElement2 = document.getElementById('dragElement2');
+
+const applyDrag = (element) => {
+    const pos1, pos2, pos3, pos4;
+    const getHeader = document.getElementById(element + 'Header');
+
+    if( getHeader ) { // if header is present
+        getHeader.onmousedown
+    }   
+
+    const dragMouseDown = () => {
+
+    }
+}
