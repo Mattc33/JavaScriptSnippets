e6fcf810f1177095eb43f8ea1e9bffabfd2d64ea
Matt Chan <m.chan@obietechologies.com>
2018-08-10 16:38:12 -0400

    pdf maker library test

---
+++ Promises/coding-train-promse-ex-1.js
@@ -0,0 +1,9 @@
+// ! promise an object that can be in a certain state
+// ? pending, waiting for data
+// ? fufilled, successfully resolved
+// ? rejected, some error has occured
+
+// * .then() receives a fn to be executed when it has been fufilled
+
+// * .catch() receives a fnto be executed when it has been rejected
+
