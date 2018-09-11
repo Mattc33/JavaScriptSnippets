945137afd2c03cebac5b2a988c4d9258ba6e5854
Matt Chan <m.chan@obietechologies.com>
2018-09-11 14:59:17 -0400

    linked list example

---
+++ DataStructures/LinkedList/LinkedList.js
@@ -0,0 +1,4 @@
+// a linked list is a linear collection of data elements
+// Order is not given a physical placement/index in memory like an array
+// raher each data element points to the next down the line in a 'linked list'
+
